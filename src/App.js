f13f1190d8d5c56d4c72088e3b9a3aa8d9a45aa0
Zahir091 <176171770+Zahir091@users.noreply.github.com>
2025-03-14 01:27:27 -0400

    Fetching movie Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import {useEffect} from 'react';
 
-const API_URL = 'http://www.omdbapi.com?apikey=c07943a';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
 const App = () => {
 
-    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data);
+    }
 
     useEffect(() => {
-
+        searchMovies('Batman');
     }, []);
 
     return (
