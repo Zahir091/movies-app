5b202eb82382c118d4c36fdb49712d657832fddb
Zahir091 <176171770+Zahir091@users.noreply.github.com>
2025-03-14 17:39:17 -0400

    added Css and saerch bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import {useEffect} from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
+const movie1 = 
+    {
+        "Title": "Batman & Robin",
+        "Year": "1997",
+        "imdbID": "tt0118688",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYzU3ZjE3M2UtM2E4Ni00MDI5LTkyZGUtOTFkMGIyYjNjZGU3XkEyXkFqcGc@._V1_SX300.jpg"
+    };
+
 const App = () => {
+
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data);
+        console.log(data.Search);
     }
 
     useEffect(() => {
@@ -16,7 +28,26 @@ const App = () => {
     }, []);
 
     return (
-        <h1>hi</h1>
+        <>
+            <div className="app">
+                <h1>Movie Land</h1>
+            </div>
+
+            <div className="search">
+                <input placeholder="Search For Movies" value="Batman"
+                onChange={() => {}}
+                />
+                <img src={SearchIcon} alt="Search" />
+            </div>
+
+            <div className='container'>
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
